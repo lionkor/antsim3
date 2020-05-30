f6cd8aeb6b9ce4347159bd0c12d36f70d1a08bac
Lion Kortlepel <development@kortlepel.com>
2020-05-30 20:28:06 +0200

    Add Mutexed

---
+++ Utils/Mutexed.h
@@ -0,0 +1,38 @@
+#ifndef MUTEXED_H
+#define MUTEXED_H
+
+#include <mutex>
+
+template<class T>
+class Mutexed
+{
+private:
+    std::mutex m_mutex;
+    T          m_value;
+
+public:
+    Mutexed(T&& value)
+        : m_value(std::move(value)) { }
+
+    ~Mutexed() {
+        m_mutex.unlock();
+    }
+
+    void lock() {
+        m_mutex.lock();
+    }
+
+    bool try_lock() {
+        return m_mutex.try_lock();
+    }
+
+    void unlock() {
+        m_mutex.unlock();
+    }
+
+    std::mutex& mutex() { return m_mutex; }
+    
+    T& value() { return m_value; }
+};
+
+#endif // MUTEXED_H
