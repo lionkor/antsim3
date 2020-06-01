579f0f1aba9d8c41b56a85535837c2c46f83bff1
Lion Kortlepel <development@kortlepel.com>
2020-06-01 18:47:25 +0200

    LazyFile: add path(), filename() getters, reorder methods

--- Utils/LazyFile.h
+++ Utils/LazyFile.h
@@ -31,18 +31,21 @@ public:
     LazyFile(const LazyFile&) = delete;
     LazyFile& operator=(const LazyFile&) = delete;
 
-    [[nodiscard]] Result<bool>                                              force_reload();
+
     [[nodiscard]] Result<std::reference_wrapper<std::vector<std::uint8_t>>> get();
 
     bool        is_valid() const { return m_validation_result.ok(); }
     std::string validation_error_message() const { return m_validation_result.message(); }
-
     std::size_t size_on_disk() const { return m_stat.st_size; }
+
+    [[nodiscard]] Result<bool> force_reload();
+    [[nodiscard]] Result<bool> restat();
+
+    const std::filesystem::path& path() const { return m_path; }
+    std::filesystem::path        filename() const { return m_path.filename(); }
 
     /// Whether the file has changed on disk. update the LazyFile with restat()
     bool has_changed_on_disk() const;
-
-    [[nodiscard]] Result<bool> restat();
 
     // Object interface
 public:
