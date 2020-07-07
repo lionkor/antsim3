e183152bc80a42f213f1a90d5fe77339f344f60b
Lion Kortlepel <development@kortlepel.com>
2020-07-07 22:29:51 +0200

    Add license notice to more files

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,3 +1,8 @@
+﻿// Copyright (c) Lion Kortlepel 2020
+// This software is free software and licensed under GPL-3.0.
+// You should have received a copy of the GNU General Public License along 
+// with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #include "Engine.h"
 
 static constexpr size_t index_3to1(size_t x, size_t y, size_t z, size_t height, size_t depth) {
